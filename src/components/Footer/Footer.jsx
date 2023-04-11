cb756bf261522fba7f6329f7d781fcc468d83057
Graimi <113901525+Graimi@users.noreply.github.com>
2023-04-11 18:47:35 +0200

    new onHandle prototipe footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,40 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Footer.css';
 
 function Footer() {
+  const [rrss, setRrss] = useState(null);
+
   return (
     <footer>
-      <article className="contact">
-        <a href="https://github.com/Graimi" target="_blank" rel="noopener noreferrer">
-          <img
-            src="https://res.cloudinary.com/dwsffp1eq/image/upload/v1679591861/NASA/github_qlykzr.png"
-            alt="Github"
-          />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/floreslujan/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://res.cloudinary.com/dwsffp1eq/image/upload/v1679591861/NASA/linkedin_gbpe7u.png"
-            alt="Linkedin"
-          />
-        </a>
-        <a href="638560723" target="_blank" rel="noopener noreferrer">
-          <img
-            src="https://res.cloudinary.com/dwsffp1eq/image/upload/v1679591861/NASA/telefono_smjgow.png"
-            alt="Teléfono"
-          />
-        </a>
-        <a href="jaimefloreslujan@gmail.com" target="_blank" rel="noopener noreferrer" onFocus="alret('Hello!')">
-          <img
-            src="https://res.cloudinary.com/dwsffp1eq/image/upload/v1679591861/NASA/email_bgpahe.png"
-            alt="Email"
-          />
-        </a>
-      </article>
-      <span className="circleDecoration" />
+      <div className="rrss">
+        <p className="info-rrss" style={{ display: rrss ? 'flex' : 'none' }}>
+          {rrss}
+        </p>
+        <article className="contact">
+          <a
+            href="https://github.com/Graimi"
+            target="_blank"
+            rel="noopener noreferrer"
+            onMouseEnter={() => setRrss('Graimi')}
+            onMouseLeave={() => setRrss(null)}
+          >
+            <img
+              src="https://res.cloudinary.com/dwsffp1eq/image/upload/v1679591861/NASA/github_qlykzr.png"
+              alt="Github"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/floreslujan/"
+            target="_blank"
+            rel="noopener noreferrer"
+            onMouseEnter={() => setRrss('floreslujan')}
+            onMouseLeave={() => setRrss(null)}
+          >
+            <img
+              src="https://res.cloudinary.com/dwsffp1eq/image/upload/v1679591861/NASA/linkedin_gbpe7u.png"
+              alt="Linkedin"
+            />
+          </a>
+          <a
+            href="638560723"
+            target="_blank"
+            rel="noopener noreferrer"
+            onMouseEnter={() => setRrss('638560723')}
+            onMouseLeave={() => setRrss(null)}
+          >
+            <img
+              src="https://res.cloudinary.com/dwsffp1eq/image/upload/v1679591861/NASA/telefono_smjgow.png"
+              alt="Teléfono"
+            />
+          </a>
+          <a
+            href="jaimefloreslujan@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            onMouseEnter={() => setRrss('jaimefloreslujan@gmail.com')}
+            onMouseLeave={() => setRrss(null)}
+          >
+            <img
+              src="https://res.cloudinary.com/dwsffp1eq/image/upload/v1679591861/NASA/email_bgpahe.png"
+              alt="Email"
+            />
+          </a>
+        </article>
+      </div>
+      <div className="circleDecoration" />
     </footer>
   );
 }
